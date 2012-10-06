30cdfdb0f741c179ff485460c5f9a02a76cac499
Kari Daggs <karidaggs@gmail.com>
2012-10-06 12:01:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkeep::Application.config.secret_token = 'c807412a97159d1f072c44d8053e1cb7d864c2f55a7648dec577974c2647c6306de743a1ac621d5f9c3a5e5273afeb25877c88212604f18dc1f1c7619e9fb62c'
